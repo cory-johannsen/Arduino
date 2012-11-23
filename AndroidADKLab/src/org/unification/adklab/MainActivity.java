1c7b62144ba92d2b6884db38e76233b8fcf57dfc
Cory Johannsen <cory.a.johannsen@gmail.com>
2012-11-22 22:08:59 -0800

    ADK communication functional sending from Android->Arduino

--- AndroidADKLab/src/org/unification/adklab/MainActivity.java
+++ AndroidADKLab/src/org/unification/adklab/MainActivity.java
@@ -16,32 +16,30 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-	
-	private Transmitter mTransmitter;
+
+    private Transmitter mTransmitter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         Button startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				start();
-			}
-        	
+            public void onClick(View v) {
+                start();
+            }
+
         });
-        
+
         Button stopButton = (Button) findViewById(R.id.stopButton);
         stopButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				stop();
-			}
-        	
+            public void onClick(View v) {
+                stop();
+            }
+
         });
     }
 
@@ -50,55 +48,57 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
-    void start() {
-    	mTransmitter = new Transmitter();
-    	new Thread(mTransmitter).start();
-    }
-    
-    void stop() {
-    	mTransmitter.halt();
-    	mTransmitter = null;
-    }
-    
-    class Transmitter implements Runnable {
-    	private boolean mRunning = true;
-    		
-		@Override
-		public void run() {
-			UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-			UsbAccessory[] accessoryList = manager.getAccessoryList();
-			UsbAccessory accessory = accessoryList[0];
-			ParcelFileDescriptor fd = manager.openAccessory(accessory);
-			if (fd != null) {
-			    InputStream inputStream = new ParcelFileDescriptor.AutoCloseInputStream(fd);
-			    OutputStream outputStream = new ParcelFileDescriptor.AutoCloseOutputStream(fd);
-			    byte counter = 0;
-				while (mRunning) {
-					byte[] buffer = {counter};
-					try {
-						outputStream.write(buffer);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					counter++;
-				}
-				try {
-					inputStream.close();
-					outputStream.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
 
-		}
-		
-		synchronized
-		public void halt() {
-			mRunning = false;
-		}
-    	
+    void start() {
+        mTransmitter = new Transmitter();
+        new Thread(mTransmitter).start();
+    }
+
+    void stop() {
+        mTransmitter.halt();
+        mTransmitter = null;
+    }
+
+    class Transmitter implements Runnable {
+        private boolean mRunning = true;
+
+        public void run() {
+            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+            UsbAccessory[] accessoryList = manager.getAccessoryList();
+            UsbAccessory accessory = accessoryList[0];
+            ParcelFileDescriptor fd = manager.openAccessory(accessory);
+            if (fd != null) {
+                InputStream inputStream = new ParcelFileDescriptor.AutoCloseInputStream(
+                        fd);
+                OutputStream outputStream = new ParcelFileDescriptor.AutoCloseOutputStream(
+                        fd);
+                byte counter = 0;
+                while (mRunning) {
+                    byte[] buffer = { counter };
+                    try {
+                        outputStream.write(buffer);
+                    }
+                    catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    counter++;
+                }
+                try {
+                    inputStream.close();
+                    outputStream.close();
+                }
+                catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        synchronized public void halt() {
+            mRunning = false;
+        }
+
     }
 }
